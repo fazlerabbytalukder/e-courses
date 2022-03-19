df996ea884fbf13bd70480ed15bce72981f21909
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-03-19 10:32:50 +0600

    courses complete

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './Course.css';
 
-const Course = () => {
+const Course = ({ course }) => {
+    const { ProjectName, ProjectDetails, img, star } = course;
     return (
         <div>
-            <h1>this is course</h1>
+            <div class="col card-height mb-4">
+                <div class="card h-100 card-background-pc">
+                    <img src={img} class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title text-center my-3">{ProjectName}</h5>
+                        <small class="card-text text-center">{ProjectDetails}</small>
+                    </div>
+                    <div class="card-footer d-flex justify-content-center align-items-center">
+                        <button className='button2'>Learn More</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Course from '../Course/Course';
+import Navbar from '../Navbar/Navbar';
 import './Courses.css';
 
 const Courses = () => {
+    const [courses, setSourses] = useState([]);
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/fazlerabbytalukder/e-course-data/main/Courses.json')
+            .then(res => res.json())
+            .then(data => setSourses(data));
+    }, [])
     return (
         <div>
-            <h1>this is courses</h1>
+            <Navbar></Navbar>
+            <div className="container">
+                <div className='text-center my-5'>
+                    <h2>Our Courses</h2>
+                </div>
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    {
+                        courses.map(course => <Course key={course.id} course={course}></Course>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
